9d1258e8215ee951b57dab2c3f93ce70634195d6
Ogunjimi Yusuf <yusuf.ogunjimi@ft.com>
2016-12-16 12:22:51 +0000

    First commit

---
+++ src/test/java/com/yusuf/AppTest.java
@@ -0,0 +1,23 @@
+package com.yusuf;
+
+import org.junit.Assert;
+import org.junit.test;
+
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+	
+	@Test
+	public void testLengthOfTheUniqueKey() {
+		App obj = new App();
+		Assert.assertEquals(36,obj.generateUniqueKey().length());
+	}
+	
+}
+
+
+
+
